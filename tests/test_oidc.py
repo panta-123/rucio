ca38f2e7d9f29bdf0934d2305f540bfe26192b19
Anil Panta <panta@jlab.org>
2025-02-12 13:01:48 -0500

    same client

--- tests/test_oidc.py
+++ tests/test_oidc.py
@@ -696,7 +696,7 @@ mock_idpsecrets_multi_vo = {
         "user_auth_client": [
             {
                 "issuer": "https://mock-oidc-provider",
-                "client_id": "mock-client-id2",
+                "client_id": "mock-client-id",
                 "client_secret": "secret",
                 "redirect_uris": "https://redirect.example.com",
                 "issuer_nickname": "example_issuer2"
